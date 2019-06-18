09789418adf85b81f7c041a0364b4e9036615fac
Sebastian Muir-Smith <sebastian@muirsmith.com>
2019-06-18 23:25:19 +1000

    add full revamp and working get/post for posts

--- Controllers/PostController.cs
+++ Controllers/PostController.cs
@@ -3,48 +3,78 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using MongoDB.Bson;
 using ThreePointSix.DBConnectors;
 using ThreePointSix.Models;
 
-namespace ThreePointSix.Controllers
-{
+namespace ThreePointSix.Controllers {
     [Route("api/[controller]")]
     [ApiController]
-    public class PostController : ControllerBase
-    {
+    public class PostController : ControllerBase {
         private readonly PostDBService _PostDBService;
 
-        public PostController(PostDBService service){
+        public PostController(PostDBService service) {
             _PostDBService = service;
         }
-        // GET api/Posts
-        [HttpGet]
-        public ActionResult<List<Post>> Get() =>
-        _PostDBService.Get();
 
-        // GET api/values/5
-        [HttpGet("{id}")]
-        public ActionResult<string> Get(int id)
-        {
-            return "value";
+        // GET api/Posts
+        // Returns <11 top posts
+        [HttpGet]
+        public ActionResult<List<Post>> Get() {
+            // Pre-Sorted post list
+            List<Post> allPosts = _PostDBService.Get();
+
+            // Only return top 10 or less posts
+            int numberOfPostsToGet = Math.Min(allPosts.Count,10);
+
+            return Models.Post.GetTopXPosts(allPosts,numberOfPostsToGet);
         }
 
         // POST api/values
+        // Creates a new post with the given post JSON object
         [HttpPost]
-        public void Post([FromBody] string value)
-        {
+        public void Post([FromBody] Post postIn) {
+
+            // Pre-Sorted post list
+            List<Post> allPosts = _PostDBService.Get();
+
+            // Extract messages and convert to lowercase
+            List<string> allPostMessages = allPosts.ConvertAll(post => post.Message.ToLower());
+
+            // Check if message allready exists
+            int foundIndex = allPostMessages.BinarySearch(postIn.Message.ToLower());
+
+            // If not found then make new post object
+            Post postOut;
+            if (foundIndex == -1) {
+                // Deep clone the old object's message
+                postOut = new Post();
+                postOut.Message = postIn.Message;
+            } else {
+                // Use the prexisting post object if found
+                postOut = allPosts[foundIndex];
+                postOut.SubmissionCount++;
+            }
+
+            postOut.Dates.Add(new BsonDateTime(DateTime.Now));
+            // First index of the input array is the name of message sender
+            postOut.Authors.Add(postIn.Authors[0]);
+
+            // Update the previous post entry if the message allready exists
+            if (foundIndex == -1){
+            _PostDBService.Create(postOut);
+            }
+            else {
+                _PostDBService.Update(postOut.Id,postOut);
+            }
         }
 
         // PUT api/values/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
+        public void Put(int id, [FromBody] string value) { }
 
         // DELETE api/values/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
+        public void Delete(int id) { }
     }
 }
--- Models/Post.cs
+++ Models/Post.cs
@@ -1,18 +1,86 @@
+using System;
+using System.Collections.Generic;
 using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
+using System.Linq;
 
-namespace ThreePointSix.Models
-{
-    public class Post
-    {
+namespace ThreePointSix.Models {
+    public class Post {
         [BsonId]
         [BsonRepresentation(BsonType.ObjectId)]
         public string Id { get; set; }
 
-        public BsonArray Dates { get; set; }
+        public List<BsonDateTime> Dates { get; set; }
 
-        public BsonString Message { get; set; }
+        [BsonRepresentation(BsonType.String)]
+        public string Message { get; set; }
 
-        public BsonArray Authors { get; set; }
+        public List<string> Authors { get; set; }
+
+        // Number of times post has been submitted
+        public int SubmissionCount { get; set; }
+
+        public Post() {
+            // Set default values
+            Dates = new List<BsonDateTime>();
+            Authors = new List<string>();
+            SubmissionCount = 1;
+        }
+
+        // Returns a sorted list of the most popular 10 posts
+        // Only more efficient than just doing posts.sort[0..numToGet] if
+        // numToGet << number of posts
+        public static List<Post> GetTopXPosts(List<Post> posts, int numToGet) {
+
+            // Not using a dictionary here due to inability to search easily
+            int[] topXIndices = new int[numToGet];
+            int[] topXValues = new int[numToGet];
+
+            int minPopularity = Int32.MaxValue;
+            // Pre-loop inits
+            Post currentPost;
+            int currentSubCount;
+
+            // Get the top 10 indices and values
+            for (int i = 0; i < posts.Count; i++) {
+
+                currentPost = posts[i];
+                currentSubCount = currentPost.SubmissionCount;
+
+                // For the first numToGet posts, the index object is not yet full, so indices can be added
+                // without checking
+                if (i < numToGet) {
+                    topXIndices[i] = i;
+                    topXValues[i] = currentSubCount;
+
+                    minPopularity = minPopularity < currentSubCount ? minPopularity : currentSubCount;
+
+                    // Sort the array on the last addition
+                    if (i == numToGet -1){
+                        Array.Sort(topXValues, topXIndices);
+                    }
+                }
+                else if (currentSubCount > minPopularity){
+                    
+                    // Remove lowest popularity entry and replace with new value
+                    topXValues[0] = currentSubCount;
+                    topXIndices[0] = i;
+
+                    // Resort array
+                    Array.Sort(topXValues, topXIndices);
+                    minPopularity = topXValues[0];
+                }
+            }
+
+            // Make new output array that contains only the top numToGet posts
+            List<Post> postsOut = new List<Post>();
+
+            for (int i = numToGet -1; i > -1; i--){
+                postsOut.Add(posts[topXIndices[i]]);
+            }
+
+            return postsOut;
+        }
+
     }
 }
